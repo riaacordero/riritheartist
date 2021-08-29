834dbb0b421b0e254156a8fdca6909e55c9494c9
Riri Cordero <41848305+Riricchan@users.noreply.github.com>
2021-08-29 12:08:44 +0800

    Added Inter font family. Minor landing page mods.

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 export default function Landing() {
     return (
         <main className="main__bg">
-            <div className="bg-img contents flex top-high bottom-high">
+            <div className="bg-white-b bg-img contents flex top-high bottom-high">
                 <div className="social_icons flex column">
                     <a href="https://behance.net/riritheartist"><img src="/images/socials/behance-black.png" alt="behance"/></a>
                     <a href="https://github.com/riaacordero"><img src="/images/socials/github-black.png" alt="github"/></a>
@@ -20,13 +20,17 @@ export default function Landing() {
                 <h2 className="top-high color--blue"> Here's what I do: </h2>
                 <p className="par--center bottom-high"> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam turpis arcu ipsum congue aliquet. Euismod ultrices est at felis metus auctor in quam pellentesque. Mauris pellentesque ultricies elementum amet. Fermentum magna a tincidunt sodales scelerisque lacus vitae. Eu, feugiat gravida et dolor habitasse porta luctus sit felis. </p>
                 <div className="project--gallery container box flex contents">
+                    {/* PLACEHOLDER */}
+                    {/* <img src="/images/avatar.png" alt="avatar" className="avatar" />
                     <img src="/images/avatar.png" alt="avatar" className="avatar" />
-                    <img src="/images/avatar.png" alt="avatar" className="avatar" />
-                    <img src="/images/avatar.png" alt="avatar" className="avatar" />
+                    <img src="/images/avatar.png" alt="avatar" className="avatar" /> */}
                 </div>
             </div>
             
             <div className="bg-img contents flex top-high bottom-high">
+                {/* <h2 className="top-high color--blue"> Connect with me!</h2>
+                <p className="bottom-high">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam turpis arcu ipsum congue aliquet. Euismod ultrices est at felis metus auctor in quam pellentesque. Mauris pellentesque ultricies elementum amet. Fermentum magna a tincidunt sodales scelerisque lacus vitae. Eu, feugiat gravida et dolor habitasse porta luctus sit felis.</p> */}
+
                 {/* This is for the contact card. */}
             </div>
         </main>
