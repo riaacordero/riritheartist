36fa2a2f4691c6ca40c3c05b9365c92fc5fb9a78
Riri Cordero <41848305+Riricchan@users.noreply.github.com>
2021-09-01 14:21:35 +0800

    'face reveal' show/hide functionality enabled

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,14 +1,16 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { SocialIcons } from './page-items/SocialIcons'
 import { LinkButton } from './page-items/Button/LinkButton'
 
 export default function Main() {
+    const [show, showClick] = useState(true)
     return (
         <main className="main__bg" id="main">
             <div className="bg-img contents flex top-high bottom-high">
                 <div className="display--avatar avatar flex column">
-                    <img src="/images/avatar-art.png" alt="avatar"/>
-                    <LinkButton>face reveal</LinkButton>
+                    {/* SHOW/HIDE feature added. To be modified for later. */}
+                    {show? <img src="/images/avatar-art.png" alt="avatar"/>:null}
+                    <LinkButton onClick ={()=> showClick(!show)}>face reveal</LinkButton>
                 </div>
                 <div className="intro column bg--center">
                     <h1 className="color--white bottom-low align-center">Hello, <br></br> I am Riri!</h1>
