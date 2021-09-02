6c0ddb5f04f6d71c6d4c13067e23b614941a54ff
Riri Cordero <41848305+Riricchan@users.noreply.github.com>
2021-09-02 20:14:33 +0800

    Fixed input submission for contact form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -12,31 +12,31 @@ export default function Contact() {
         <div className="contents flex column top-high bottom-high">
                 <h2 className="color--black par--center">Let's Talk!</h2>
                 <div className="par--center flex">
-                    <p className="short--width">Connect with me through this contact form</p>
+                    <p className="short--width">Got anything in mind or you just want to say hi? Send me a message through this contact form:</p>
                 </div>
                 <form className ="forms flex" onSubmit={handleSubmit}>
                     <FormGroup>
                         <div>
-                            <input className="wide" id="name" placeholder="Name" type="name" name="name"/>
+                            <input className="wide" id="name" placeholder="Name" type="name" name="name" required/>
                             <ValidationError prefix="Name" field="Name" errors={state.errors}/>
                         </div>
                     </FormGroup>
                     <FormGroup>
                         <div>
-                            <input className="wide" id="email" placeholder="Email Address" type="email" name="email"/>
+                            <input className="wide" id="email" placeholder="Email Address" type="email" name="email" required/>
                             <ValidationError prefix="Email" field="Email" errors={state.errors}/>
                         </div>
                     </FormGroup>
                     <FormGroup>
                         <div>
-                            <textarea className="wide--height-overidden" id="message" placeholder="Message" name="message"/>
+                            <textarea className="wide--height-overidden" id="message" placeholder="Message" name="message" required/>
                             <ValidationError prefix="Message" field="Message" errors={state.errors}/>
                         </div>
                     </FormGroup>
                     <Button type="submit" disabled={state.submitting}>submit</Button>
-                    
+
                     {state.succeeded === true && <p className="success-msg"> Email sent successfully! </p>}
-                    {state.succeeded === false && <p className="error-msg"> Please try again! </p>}
+                    
                 </form>
             </div>
     </main>
