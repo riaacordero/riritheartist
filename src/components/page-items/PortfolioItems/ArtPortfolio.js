d017f85b37cf0668bef26cf09de88245f4102999
Riri Cordero <41848305+Riricchan@users.noreply.github.com>
2021-09-05 11:50:19 +0800

    Added portfolio items for development

---
+++ src/components/page-items/PortfolioItems/ArtPortfolio.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+export default function ArtPortfolio() {
+    return(
+        <main className="main__bg" id="portfolio">
+            <div className="bg-img contents flex top-high bottom-high">
+                <h3 className="color--white about par--center">[Art Portfolio goes here]</h3>
+            </div>
+        </main> 
+    )
+}
---
+++ src/components/page-items/PortfolioItems/DevPortfolio.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+export default function DevPortfolio() {
+    return(
+        <main className="main__bg" id="portfolio">
+            <div className="bg-img contents flex top-high bottom-high">
+                <h3 className="color--white about par--center">[Developer Portfolio goes here]</h3>
+            </div>
+        </main> 
+    )
+}
